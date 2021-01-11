7d88f084c3db1bf0d7018cb181490c44c1c63331
wandagarbocz <59895661+wandagarbocz@users.noreply.github.com>
2021-01-11 03:19:11 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,21 @@
-#include "spi.h"
-#include "tpm.h"
 #include "frdm_bsp.h" 
 #include "tsi.h"
+#include "spi.h"
+#include "tpm.h"  
 
 void SysTick_Handler(void);
-void wait(void);
+void press_and_play_right(uint8_t, uint8_t);
+void press_and_play_left(uint8_t, uint8_t);
+void press_and_play_middle(uint8_t, uint8_t);
+void hold_and_play_right(uint8_t);
+
 
 static uint8_t slider = 0;
 static uint8_t msTicks = 0;
 static uint8_t newTick = 0;
+static uint8_t liczba_p=0;
+static uint8_t liczba_s=1;
+
 
 const unsigned char logo_mini_mono [] = {
 0xFC, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -45,41 +52,118 @@ const unsigned char logo_mini_mono [] = {
 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x7F, 0x3F,
 };
 
+int main (void) { 
+	
 
-int main (void)
-{
-	TSI_Init(); //inicjalizacja slidera
-	SysTick_Config(SystemCoreClock/1000);
+	TSI_init(); //inicjalizacja slidera
+	SysTick_Config(1000000); //inicjalizacja timera
+	TPM0_Init(); 
+	
 	tpm1_init_pwm();
-	TPM0_Init(); //inicjalizacja przerwania
 	spi_init();
 	spi_write_data(logo_mini_mono);
-	while(1)
-	{
+	
+	while(1){
 		__WFI();	//czekanie na przerwanie
+		
 		if (newTick==1){ //jezeli bylo przerwanie
-			if( msTicks%2 == 0 ) {
-			slider=TSI_ReadSlider(); //odczytanie wartosci ze slidera
-			if(slider>0){
-				TPM0_Play(); //wlaczenie piosenki
-				newTick=0; //wyzerowanie zmiennej informujacej o przerwaniu
-				//wait(); //wywolanie funkcji realizujacej opoznienie
-				//TPM0_Pause(); //pauzowanie piosenki
-			}
+			slider=TSI_ReadSlider();
+			liczba_p=liczba_s%6;
+			press_and_play_right(slider, liczba_p);
+			liczba_s=liczba_s+1;
 		}
 	}
-		//	spi_write(logo_mini_mono);
+}
+
+		
+void press_and_play_right(uint8_t slider1, uint8_t liczba1){
+	if (slider1>66){
+		if (liczba1==0){
+			TPM0_Play2();
+		}
+		else if (liczba1==1){
+			TPM0_Play3();
+		}
+		
+		else if (liczba1==2){
+			TPM0_Play4();
+		}
+		else if (liczba1==3){
+			TPM0_Play5();
+		}
+		else if(liczba1==4){
+			TPM0_Play6();
+		}
+
+	}
+
+		
+}
+	
+void press_and_play_middle(uint8_t slider1, uint8_t liczba1){
+	if (slider1>=33 && slider1<=66){
+		if (liczba1==0){
+			TPM0_Play2();
+		}
+		else if (liczba1==1){
+			TPM0_Play3();
+		}
+		
+		else if (liczba1==2){
+			TPM0_Play4();
+		}
+		else if (liczba1==3){
+			TPM0_Play5();
+		}
+		else if(liczba1==4){
+			TPM0_Play6();
+		}
+	
+	}
+	}
+
+
+void press_and_play_left(uint8_t slider1, uint8_t liczba1){
+	if (slider1>0 && slider1<33){
+		if (liczba1==0){
+			TPM0_Play2();
+		}
+		else if (liczba1==1){
+			TPM0_Play3();
+		}
+		else if (liczba1==2){
+			TPM0_Play4();
+		}
+		else if(liczba1==3){
+			TPM0_Play5();
+		}
+		else if(liczba1==4){
+			TPM0_Play6();
+		}
+
+	
 	}
 }
+
+void hold_and_play_right(uint8_t slider1){
+	uint8_t zmienna=0;
+	uint8_t slider2=0;
+	if (slider1>66){
+		zmienna=1;
+		
+		if (zmienna==1){
+			slider2=TSI_ReadSlider();
+			if (slider2<=0){
+			zmienna=0;
+			TPM0_Play2();
+				}
+			}
+		}
+		}
 
 void SysTick_Handler(void) {
 	msTicks++;
 	newTick = 1;
 }
 
-void wait(void){
-	for(volatile int k=0; k<=3600; k++){
-		for(volatile int i=0; i<=600; i++){
-		}
-	}
-}
+
