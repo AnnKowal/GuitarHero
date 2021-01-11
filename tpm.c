7fc9bcb7fc3afcc16011d12065e33a1a3ae50d99
wandagarbocz <59895661+wandagarbocz@users.noreply.github.com>
2021-01-11 03:19:55 +0100

    Add files via upload

--- tpm.c
+++ tpm.c
@@ -3,14 +3,19 @@
 
 
 
+void TPM0_IRQHandler(void);
+
 static uint8_t enable = 0;
 static uint32_t upSampleCNT = 0;
+static uint32_t upSampleCNT2 = 0;
 static uint32_t probka = 0;
 static uint32_t probka_do_odt = 0;
-static uint32_t upsampling = 10;
+static uint32_t upsampling = 20;
+static uint32_t upsampling2 = 40;
 static uint8_t  play = 1;
-static uint8_t  pause1 = 0;
-static uint8_t  pause = 0;
+static uint8_t  liczba= 0;
+static uint8_t pause=0;
+
 
 void tpm1_init_pwm(void)
 {
@@ -26,14 +31,15 @@ void tpm1_init_pwm(void)
 	TPM1->CONTROLS[0].CnV = 10;
 
 }
-	
+
+
 void TPM0_Init(void) {
 			
   SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;		//wlaczenie maski 
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);    //wybor zrodla zegara
 
 	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK; //podlaczenie zegara do portu B
-	PORTB->PCR[6] = PORT_PCR_MUX(2);		//wybor multipleksera	
+	PORTB->PCR[7] = PORT_PCR_MUX(2);		//wybor multipleksera	
 	
 	TPM0->SC |= TPM_SC_PS(2);  	//ustawienie prescalera 				  
 	TPM0->SC |= TPM_SC_CMOD(1);	//wybor zegara				  
@@ -61,32 +67,62 @@ void TPM0_Init(void) {
 	enable= 1; 
 }
 
-void TPM0_Play(void) {   
-	play = 1;
+
+
+void TPM0_Play2(void) {   
+
+	play=1;
+	probka=0;
+	liczba=2;
 	pause=0;
-	
-	if (pause1==1){
-	probka=probka_do_odt;
-	}
-	else{
-		probka=0;
-	}
-	pause1=0;
 }
+void TPM0_Play3(void) {   
+
+	play=1;
+	probka=0;
+	liczba=3;
+		pause=0;
+}
+
+void TPM0_Play4(void) {   
+
+	play=1;
+	probka=0;
+	liczba=4;
+		pause=0;
+}
+void TPM0_Play5(void) {   
+
+	play=1;
+	probka=0;
+	liczba=5;
+		pause=0;
+}
+void TPM0_Play6(void) {   
+
+	play=1;
+	probka=0;
+	liczba=6;
+		pause=0;
+}
+
+
 
 void TPM0_Pause(void){
-	pause=1;
 	play=0;
-	pause1=1;
-	
+	pause=1;
 }
-
 void TPM0_IRQHandler(void) {
+	if (probka > dlugosc ) {
+			play = 0;         
+			TPM0->CONTROLS[2].CnV = 0;
 	
-	if (play==1 && pause==0) {
+		}
+		
+		if (play==1 && liczba==2) {
 		if (upSampleCNT == 0){
 			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka[probka_do_odt]; 
+			TPM0->CONTROLS[2].CnV = piosenka2[probka_do_odt]; 
 		}	
 		if (probka > dlugosc ) {
 			play = 0;         
@@ -98,11 +134,76 @@ void TPM0_IRQHandler(void) {
 		{
 			upSampleCNT = 0;
 		}
-	}
-		if (pause==1 ) {         
-			TPM0->CONTROLS[2].CnV = 0;
-			pause=0;
 		}
-			
+	
+		if (play==1 && liczba==3) {
+		if (upSampleCNT == 0){
+			probka_do_odt=probka++;
+			TPM0->CONTROLS[2].CnV = piosenka3[probka_do_odt]; 
+		}	
+		if (probka > dlugosc ) {
+			play = 0;         
+			TPM0->CONTROLS[2].CnV = 0;
+	
+		}
+	
+		if (++upSampleCNT > (upsampling-1)) 
+		{
+			upSampleCNT = 0;
+		}
+		}
+		
+		if (play==1 && liczba==4) {
+		if (upSampleCNT == 0){
+			probka_do_odt=probka++;
+			TPM0->CONTROLS[2].CnV = piosenka4[probka_do_odt]; 
+		}	
+		if (probka > dlugosc ) {
+			play = 0;         
+			TPM0->CONTROLS[2].CnV = 0;
+	
+		}
+	
+		if (++upSampleCNT > (upsampling-1)) 
+		{
+			upSampleCNT = 0;
+		}
+		}
+		
+		if (play==1 && liczba==5) {
+		if (upSampleCNT == 0){
+			probka_do_odt=probka++;
+			TPM0->CONTROLS[2].CnV = piosenka5[probka_do_odt]; 
+		}	
+		if (probka > dlugosc ) {
+			play = 0;         
+			TPM0->CONTROLS[2].CnV = 0;
+	
+		}
+	
+		if (++upSampleCNT > (upsampling-1)) 
+		{
+			upSampleCNT = 0;
+		}
+		}
+		
+		if (play==1 && liczba==6) {
+		if (upSampleCNT == 0){
+			probka_do_odt=probka++;
+			TPM0->CONTROLS[2].CnV = piosenka6[probka_do_odt]; 
+		}	
+		if (probka > dlugosc ) {
+			play = 0;         
+			TPM0->CONTROLS[2].CnV = 0;
+	
+		}
+	
+		if (++upSampleCNT > (upsampling-1)) 
+		{
+			upSampleCNT = 0;
+		}
+		}
+
+		
 	TPM0->CONTROLS[0].CnSC |= TPM_CnSC_CHF_MASK; 
 }
--- tpm.h
+++ tpm.h
@@ -1,11 +1,10 @@
-  
 #ifndef TPM_H
 #define TPM_H
 
 #include "frdm_bsp.h"
 
-void tpm1_init_pwm(void);
 
+void tpm1_init_pwm(void);
 
 void TPM0_Init(void);
 void TPM0_Pause(void);
@@ -15,5 +14,5 @@ void TPM0_Play3(void);
 void TPM0_Play4(void);
 void TPM0_Play5(void);
 void TPM0_Play6(void);
-
+//void TPM0_background(void);
 #endif 
