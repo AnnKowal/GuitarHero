0ddbec21d45b79ceb1b94c8c057bf04c1509174d
wandagarbocz <59895661+wandagarbocz@users.noreply.github.com>
2021-01-12 19:56:21 +0100

    Add files via upload

--- tpm.c
+++ tpm.c
@@ -1,23 +1,26 @@
 #include "tpm.h"
-#include "piosenka.h"
-
+//#include "uart.h"
 void TPM0_IRQHandler(void);
-
-
 static uint8_t enable = 0;
 static uint32_t upSampleCNT = 0;
-static uint32_t upSampleCNT2 = 0;
 static uint32_t probka = 0;
+static uint32_t dlugosc = 5000;
 static uint32_t probka_do_odt = 0;
 static uint32_t upsampling = 20;
-static uint32_t upsampling2 = 40;
 static uint8_t  play = 1;
-static uint8_t  liczba= 0;
-static uint8_t pause=0;
+static uint8_t  pause = 0;
+static uint8_t  empty=1;
+uint8_t  piosenka_FULL = 0;
+uint8_t temp=0;
+uint8_t temp2=0;
+static uint8_t piosenka2[];
+static uint8_t piosenka3[3000];
+
+
 
 void tpm1_init_pwm(void)
 {
-	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 	SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;
 	PORTA->PCR[12] = PORT_PCR_MUX(2);
@@ -26,10 +29,11 @@ void tpm1_init_pwm(void)
 	TPM1->MOD = 100;
 	TPM1->SC &= ~TPM_SC_CPWMS_MASK; 
 	TPM1->CONTROLS[0].CnSC |= (TPM_CnSC_MSB_MASK | TPM_CnSC_ELSA_MASK);
-	TPM1->CONTROLS[0].CnV = 20;
+	TPM1->CONTROLS[0].CnV = 10;
 
 }
-	
+
+
 void TPM0_Init(void) {
 			
   SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK;		//wlaczenie maski 
@@ -66,141 +70,74 @@ void TPM0_Init(void) {
 
 
 
-void TPM0_Play2(void) {   
 
-	play=1;
-	probka=0;
-	liczba=2;
-	pause=0;
+void UART0_Init(void)
+{
+	SIM->SCGC4 |= SIM_SCGC4_UART0_MASK;							
+	SIM->SCGC5 |= SIM_SCGC5_PORTB_MASK;							
+	SIM->SOPT2 |= SIM_SOPT2_UART0SRC(MCGFLLCLK);		
+	PORTB->PCR[1] = PORT_PCR_MUX(2);								
+	PORTB->PCR[2] = PORT_PCR_MUX(2);								
+	
+	UART0->C2 &= ~(UART0_C2_TE_MASK | UART0_C2_RE_MASK );		
+	UART0->BDH = 0;
+	UART0->BDL =91;		
+	UART0->C4 &= ~UART0_C4_OSR_MASK;
+	UART0->C4 |= UART0_C4_OSR(15);	
+	UART0->C5 |= UART0_C5_BOTHEDGE_MASK;	
+	UART0->C2 |= UART0_C2_RIE_MASK;		
+	UART0->C2 |= (UART0_C2_TE_MASK | UART0_C2_RE_MASK);		
 }
-void TPM0_Play3(void) {   
 
-	play=1;
-	probka=0;
-	liczba=3;
-		pause=0;
-}
+void UART0_read(){
+	if(UART0->S1 & UART0_S1_RDRF_MASK)
+	{
 
-void TPM0_Play4(void) {   
-
-	play=1;
-	probka=0;
-	liczba=4;
-		pause=0;
-}
-void TPM0_Play5(void) {   
-
-	play=1;
-	probka=0;
-	liczba=5;
-		pause=0;
+		temp=UART0->D;	
+		if(!piosenka_FULL)
+		{
+			piosenka2[0] = temp;	
+			piosenka_FULL=1;
+		}
+	}
 }
 void TPM0_Play6(void) {   
 
 	play=1;
 	probka=0;
-	liczba=6;
 		pause=0;
 }
-
-
 
 void TPM0_Pause(void){
 	play=0;
 	pause=1;
 }
 void TPM0_IRQHandler(void) {
-	if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-		
-		if (play==1 && liczba==2) {
-		if (upSampleCNT == 0){
-			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka2[probka_do_odt]; 
-		}	
-		if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-	
-		if (++upSampleCNT > (upsampling-1)) 
-		{
-			upSampleCNT = 0;
-		}
-		}
-	
-		if (play==1 && liczba==3) {
-		if (upSampleCNT == 0){
-			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka3[probka_do_odt]; 
-		}	
-		if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-	
-		if (++upSampleCNT > (upsampling-1)) 
-		{
-			upSampleCNT = 0;
-		}
-		}
-		
-		if (play==1 && liczba==4) {
-		if (upSampleCNT == 0){
-			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka4[probka_do_odt]; 
-		}	
-		if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-	
-		if (++upSampleCNT > (upsampling-1)) 
-		{
-			upSampleCNT = 0;
-		}
-		}
-		
-		if (play==1 && liczba==5) {
-		if (upSampleCNT == 0){
-			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka5[probka_do_odt]; 
-		}	
-		if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-	
-		if (++upSampleCNT > (upsampling-1)) 
-		{
-			upSampleCNT = 0;
-		}
-		}
-		
-		if (play==1 && liczba==6) {
-		if (upSampleCNT == 0){
-			probka_do_odt=probka++;
-			TPM0->CONTROLS[2].CnV = piosenka6[probka_do_odt]; 
-		}	
-		if (probka > dlugosc ) {
-			play = 0;         
-			TPM0->CONTROLS[2].CnV = 0;
-	
-		}
-	
-		if (++upSampleCNT > (upsampling-1)) 
-		{
-			upSampleCNT = 0;
-		}
-		}
-
-		
-	TPM0->CONTROLS[0].CnSC |= TPM_CnSC_CHF_MASK; 
+//if(piosenka3[0]==0){	
+	while(!(UART0->S1 & UART0_S1_TDRE_MASK));
+	UART0->D = play;
+		for(uint16_t k=0; k<3000; k++){	
+						UART0_read();
+						if(piosenka_FULL==1)		
+						piosenka3[k]=temp;	
+						}
+		//empty=0;
+					//}
+				if (play==1) {
+						if (upSampleCNT == 0){
+			
+						TPM0->CONTROLS[2].CnV = piosenka3[probka_do_odt]; 
+						}
+						if (probka > dlugosc ) {
+						play = 0;         
+						TPM0->CONTROLS[2].CnV = 0;
+						}
+						if (++upSampleCNT > (upsampling-1)) 
+						{
+						upSampleCNT = 0;
+						}
+						}
+				TPM0->CONTROLS[0].CnSC |= TPM_CnSC_CHF_MASK; 
 }
+
+	
--- tpm.h
+++ tpm.h
@@ -1,18 +1,18 @@
 #ifndef TPM_H
 #define TPM_H
+#define CLK_DIS 					0x00
+#define MCGFLLCLK 				0x01
+#define OSCERCLK					0x02
+#define MCGIRCLK					0x03
 
 #include "frdm_bsp.h"
 
-
+void UART0_read(void);
+void UART0_Init(void);
 void tpm1_init_pwm(void);
 
 void TPM0_Init(void);
+void TPM0_Play6(void);
 void TPM0_Pause(void);
 
-void TPM0_Play2(void);
-void TPM0_Play3(void);
-void TPM0_Play4(void);
-void TPM0_Play5(void);
-void TPM0_Play6(void);
-//void TPM0_background(void);
 #endif 
