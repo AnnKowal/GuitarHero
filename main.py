f7360cf26f35956afc218e8203578e50935d5ad5
wandagarbocz <59895661+wandagarbocz@users.noreply.github.com>
2021-01-25 22:27:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,96 +1,68 @@
 import serial
-import time
 
 data2=[]
 data3=[]
 data4=[]
-data5=[]
-data6=[]
 data7=[]
-data8=[]
 
 
-plik=open("piosenka.txt", "r")
+plik=open("piosenka.txt", "r") #otwarcie pliku z probkami piosenki do odczytu
 
-if plik.mode=="r":
+if plik.mode=="r": #odczyt z pliku i zapis rozdzielonych wartosci do tablicy
     lines = plik.readlines()
     for line in lines:
             line = line[1:-2]
             data2.append(line.split(','))
 
 print(len(data2))
-for r in range(246000):
+for r in range(246000): 
     for c in range(12):
-            data2[r][c]=int(data2[r][c],16)
+            data2[r][c]=int(data2[r][c],16) 
             data4.append(hex(data2[r][c]))
 
 
-wartosc=30000
-
-
-
-#print(len(data3))
-#print(len(data7))
 zmienna6=b'\x05'
-zmienna8=b'\x01'
 true1=0
 true2=0
 z1=0
 z2=12000
-z3=3000
-z4=15000
-with serial.Serial('COM3', 921600) as serial1:
-    while serial1.isOpen():
-        data3.clear()
+
+with serial.Serial('COM3', 921600) as serial1: #otwarcie portu szeregowego z zadana predkoscia transmisji
+    while serial1.isOpen(): #dopoki port szeregowy jest otwarty
+        data3.clear() #wyzyszczenie tablic z probkami
         data7.clear()
         print('port otwarty')
+        
         for i in range(len(data4)):
-            if i%4==0:
+            if i%4==0: #downsapling, zmiana z czestotliwosci probkowania 8kHz na 2kHz
                 if i >= z1 and len(data3)<3000:
-                    data3.append(data4[i])
+                    data3.append(data4[i]) #wpisanie nowych, kolejnych probek do tablicy
                 elif i >= z2 and len(data7)<3000:
-                    data7.append(data4[i])
-                #elif i>=z3 and len(data8)<3000:
-                 #   data8.append(data4[i])
-       # print(len(data3))
-        #print(len(data7))
-        #print(len(data8))
-        if true1!=1:
+                    data7.append(data4[i]) #wpisanie nowych, kolejnych probek do tablicy
+                    
+        if true1!=1: #jezeli w twj iteracji petli nie przeslano zawartosci tablicy data3
             zmienna5 = serial1.read(1)
 
-            if zmienna5 == zmienna6:
+            if zmienna5 == zmienna6: #jezeli mikroprocesor przeslal zmienna tozsama z checia przyjecia probek dzwieku
                 print('wysylanie')
                 for k in range(len(data3)):
-                  #  time.sleep(0.01)
-                    serial1.write(data3[k].encode())
-                    #print(k)
-                   # time.sleep(0.01)
-                    zmienna11=serial1.read(1)
-                  #  print (k)
-                print('wyslano')
-                true2=1
+                    serial1.write(data3[k].encode()) #wysylanie danych w petli do mikroprocesora
+                    zmienna11=serial1.read(1) #odczyt danych od mikroprocesora
+              
+                true2=1  
                 true1=1
-        if true2==1:
-            zmienna7 = serial1.read(1)
+        
+        if true2==1: #jezeli w tej iteracji petli nie przeslano zawartosci tablicy data7
+            zmienna7 = serial1.read(1) #odczyt danych od mikroprocesora
             print(zmienna7)
 
-            if zmienna7 == zmienna6:
-                #time.sleep(0.1)
-                #true2=1
-                #if true2!=1:
-                for t in range(len(data7)):
-                    serial1.write(data7[t].encode())
-                    zmienna9=serial1.read(1)
-                print('odebrano')
+            if zmienna7 == zmienna6:  #jezeli mikroprocesor przeslal zmienna tozsama z checia przyjecia probek dzwieku
+                for t in range(len(data7)): 
+                    serial1.write(data7[t].encode()) #wysylanie danych w petli do mikroprocesora
+                    zmienna9=serial1.read(1) #odczyt danych od mikroprocesora
+                
                 true1 = 0
                 true2 = 0
-                z1 += 24000
+                z1 += 24000 #zwiekszenie zmiennych ktore definiuja poczatek numer elementu, ktory w kolejnej iteracji petli bedzie wysylany przez UART jako pierwszy
                 z2 += 24000
-            #zmienna11=serial1.read(1)
-            #if zmienna11 == zmienna6:
-             #   print("start")
-              #  for t in range(len(data8)):
-               #     serial1.write(data8[t].encode())
-                #    zmienna9=serial1.read(1)
-                 #   print(t)
-                #print('odebrano')
+ 
